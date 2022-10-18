57b1c0165db2eb2a9ee24da4b8284660cc8df594
但力 <88420567+dl-mac@users.noreply.github.com>
2022-10-18 08:57:53 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -86,6 +86,7 @@ class HitCarder(object):
 
         try:
             new_form = re.findall(r'<ul>[\s\S]*?</ul>', html)[0]
+            print(new_form)
         except IndexError as _:
             raise RegexMatchError('Relative info not found in html with regex')
 
